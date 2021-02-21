103bbff5f1900f0379b8f613ca1b03a81a91fc2d
Andrea Piseri <andrea.piseri@gmail.com>
2021-02-21 21:45:44 +0100

    dropped the dependency to relm, implemented deserialization, updated roadmap in README.md

--- README.md
+++ README.md
@@ -15,18 +15,52 @@ lightweight window managers in order to change settings on the fly.
 ## feature roadmap
 
 - [x] correctly render hardcoded components
-- [ ] parse components from a `json` file at startup
-- [ ] interact with provided scripts (e.g. set the correct value for the brightness slider at startup)
+- [x] parse components from a `json` file at startup
+- [] interact with provided scripts
+  - [x] execute commands (e.g. change brightness with a slider)
+  - [ ] get values back from commands (e.g. set the correct value for the brightness slider at startup)
+- [ ] add support for image widgets
+- [ ] style the layout in a sensible way
+  - [ ] allow setting names for tabs
+  - [ ] move the tab list to the side
+  - [ ] make sliders fill all horizontal available space
+
 
 ## Dependencies
 
-Dama is built with rust, using the `relm` and `gtk` crates and uses `cargo` to manage the build process.
+Dama is built with rust, using the `gtk` crate and uses `cargo` to manage the build process.
 
 ## Installation
 
 A makefile will be provided in the near future.
 
-## writing your own pages
+## writing your own menu entries
 
-This is currently only possible by modifying the source code.
-I plan to use a json parser to deserialize the layout from a `dama.json` file
+menu entries are read from a file called `dama.json` in the directory you launch the executable in,
+but in the future its default location will be `XDG_CONFIG_HOME/dama/config.json`.
+
+Available entries are of types:
+
+```json
+{"Notebook" : [/* list of children*/]},
+
+{"Box": ["Vertical", // or  Horizontal
+      [/*list of children*/]
+]},
+
+{"Label": ["some text"]},
+
+{"Button": ["the button's label", 
+    "notify-send \"click!\""]}, // the command to be executed
+
+{"Scale": [0.0,   // the minimum value
+           100.0, // the maximum value
+           "xbacklight -set"] // the command to be executed when the slider is moved.
+}                             // the current value of the slider is added to the end 
+                              // of this string, rounded to an integer.
+```
+
+all commands are executed with `sh -c`.
+
+The top-level object must be of type `Notebook`, but nesting is not restricted beyond that.
+
--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,154 @@
-use std::process::Command;
+use gtk::{ApplicationWindow, Application};
+use gtk::prelude::*;
+use gio::prelude::*;
 
-use gtk::{
-    RangeExt,
-    ButtonExt,
-    Inhibit,
-    OrientableExt,
-    WidgetExt,
-};
+use std::io::prelude::*;
+use std::fs::File;
+use serde_derive::{Serialize, Deserialize};
 
-use gtk::Orientation::Vertical;
-use relm::Widget;
-use relm_derive::{Msg, widget};
+use std::env::args;
 
-use self::Msg::*;
-
-pub struct Model {
+#[derive(Serialize, Deserialize)]
+enum Orientation {
+    Vertical,
+    Horizontal,
 }
 
-#[derive(Msg)]
-pub enum Msg {
-    Quit,
-    RunCommand(String),
+#[derive(Serialize, Deserialize)]
+enum SeralizableWidget {
+    Notebook(Vec<SeralizableWidget>),
+    Box(Orientation, Vec<SeralizableWidget>),
+    Label(String),
+    Button(String, String), // label, command
+    Scale(f64, f64, String), // min, max, command
 }
 
-#[widget]
-impl Widget for Win {
-    fn init_view(&mut self) {
-        self.widgets.brightness.set_range(0., 100.);
-        self.widgets.brightness.set_value(50.);
 
-    }
+trait Attach {
+    fn attach (&self, obj: SeralizableWidget );
+}
 
-    fn model( _: ()) -> Model {
-        Model {
-        }
-    }
-
-    fn update(&mut self, event: Msg) {
-        match event {
-            Quit => gtk::main_quit(),
-            RunCommand(command) => {
-                Command::new("sh")
-                    .arg("-c")
-                    .arg(command)
-                    .spawn().unwrap();
+impl Attach for gtk::Box {
+    fn attach(&self, obj: SeralizableWidget) {
+        match obj {
+            SeralizableWidget::Box(orientation, elements) => {
+                let nb = gtk::Box::new(match orientation {
+                    Orientation::Vertical => gtk::Orientation::Vertical,
+                    Orientation::Horizontal => gtk::Orientation::Horizontal,}
+                    , 12);
+                for element in elements {
+                    nb.attach(element);
+                }
+                self.add(&nb);
+            }
+            SeralizableWidget::Notebook(v) => {
+                let nb = gtk::Notebook::new();
+                for elem in v {
+                    nb.attach(elem);
+                }
+                self.add(&nb);
+            }
+            SeralizableWidget::Button(label, command) => {
+                let b = gtk::Button::with_label(&*label);
+                b.connect_clicked(
+                    move |_| {std::process::Command::new("sh").arg("-c")
+                        .arg(command.clone()).spawn().unwrap();
+                    });
+                self.add(&b);
+            }
+            SeralizableWidget::Label(label) => {
+                let l = gtk::Label::new(None);
+                l.set_markup(&*label);
+                l.set_line_wrap(true);
+                self.add(&l);
+            }
+            SeralizableWidget::Scale(start, end, command) => {
+                let l = gtk::Scale::with_range(
+                    gtk::Orientation::Horizontal, 
+                    start, 
+                    end, 
+                    1.);
+                l.connect_change_value(
+                    move |_, _, new_value| {std::process::Command::new("sh").arg("-c")
+                            .arg(format!("{} {}", command.clone(), new_value as i32))
+                            .spawn().unwrap();
+                        Inhibit(false)
+                    });
+                self.add(&l);
             }
         }
     }
-
-    view! {
-        gtk::Window {
-            gtk::Box {
-                orientation: Vertical,
-                gtk::Notebook {
-                    gtk::Box {
-                        orientation: Vertical,
-                        #[name="brightness"]
-                        gtk::Scale{
-                            change_value( _ , _ , new_value) => 
-                                (RunCommand(format!("echo {} > /sys/class/backlight/amdgpu_bl0/brightness", new_value as u8)), 
-                                    Inhibit(false))
-                        }
-                    },
-                    gtk::Box {
-                        orientation: Vertical,
-                        gtk::Button {
-                            label: "hello",
-                            clicked => RunCommand("notify-send Hello".to_string()),
-                        },
-                        gtk::Button {
-                            label: "world",
-                            clicked => RunCommand("notify-send World".to_string()),
-                        }
-                    },
+}
+impl Attach for gtk::Notebook {
+    fn attach(&self, obj: SeralizableWidget) {
+        match obj {
+            SeralizableWidget::Box(orientation, elements) => {
+                let nb = gtk::Box::new(match orientation {
+                    Orientation::Vertical => gtk::Orientation::Vertical,
+                    Orientation::Horizontal => gtk::Orientation::Horizontal,
+                }, 12);
+                for element in elements {
+                    nb.attach(element);
                 }
-            },
-            delete_event(_, _) => (Quit, Inhibit(false)),
+                self.add(&nb);
+            }
+            _ => {}
         }
     }
 }
 
+impl Attach for ApplicationWindow {
+    fn attach(&self, obj: SeralizableWidget) {
+        match obj {
+            SeralizableWidget::Notebook(v) => {
+                let nb = gtk::Notebook::new();
+                for elem in v {
+                    nb.attach(elem);
+                }
+                self.add(&nb);
+            }
+            _ => {}
+        }
+    }
+}
+
+
+
+fn build_ui(app: &Application, source: &str) {
+
+
+    let win = ApplicationWindow::new(app);
+    win.set_title("Dama - Menu");
+    win.set_border_width(10);
+   
+    let otherwidget = SeralizableWidget::Box(Orientation::Vertical, vec![]);
+    let s = serde_json::to_string_pretty(&otherwidget).unwrap();
+    println!("{}", s);
+    
+    win.set_position(gtk::WindowPosition::Center);
+    let mywidget: SeralizableWidget = serde_json::from_str(source).unwrap();
+//  let mywidget: SeralizableWidget = SeralizableWidget::Notebook(
+//      vec![
+//      SeralizableWidget::Box(
+//          vec! [
+//          SeralizableWidget::Button("hello".to_string(), "notify-send world".to_string()),
+//          SeralizableWidget::Label("some label".to_string()),
+//          SeralizableWidget::Scale(0., 100., "notify-send".to_string()),
+//          ])
+//      ]);
+    
+
+    win.attach(mywidget);
+    win.show_all();
+}
+
+
 fn main() {
-    Win::run(()).expect("Win::run failed");
+    let mut f = File::open("dama.json").expect("could not read from file");
+    let mut source = String::new();
+    f.read_to_string(&mut source).expect("there was an error parsing the config file");
+    let app = Application::new(Some("com.andrea.example"), Default::default())
+        .expect("Could not create a new application");
+    app.connect_activate(move |a| build_ui(a, &*source));
+    app.run(&args().collect::<Vec<_>>());
 }
